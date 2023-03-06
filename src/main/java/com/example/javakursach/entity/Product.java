bc1f74102bdc29b6546680268e3f2ab5ec409b10
p0pl3 <m.poplavskiy04@gmail.com>
2023-03-06 16:50:45 +0300

    first commit

---
+++ src/main/java/com/example/javakursach/entity/Product.java
@@ -0,0 +1,28 @@
+package com.example.javakursach.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String title;
+    private float price;
+    private String description;
+    private float rating;
+
+}
---
+++ src/test/java/com/example/javakursach/JavaKursachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javakursach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKursachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
